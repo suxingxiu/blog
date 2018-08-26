f8b50d1a490c7f58851d5f8dd583d531487d8be7
suxingxiu <1395492459@qq.com>
2018-08-26 23:19:30 +0800

    前台目录规制

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
 // 导入express框架
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 // 创建web服务器
 const app = express();
+
+// 开放静态资源目录
+// 
+app.use(express.static(path.join(__dirname, 'public')))
+
+// 配置模板引擎
+// 告诉模板引擎用哪个模板
+
+app.engine('handlebars', exphbs({
+    // 抽取公共部分
+    partialsDir: [{
+        dir: path.join(__dirname, 'views', 'home', 'partials')
+    }, {
+        dir: path.join(__dirname, 'views', 'admin', 'partials')
+    }],
+    // 抽取架构  架构目录
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    defaultLayout: 'home'
+}));
+
+// 配置模板路径
+app.set('views', path.join(__dirname, 'views'));
+// 指定模板后缀
+app.set('view engine', 'handlebars');
 
 // 路由
 
 app.get('/', (req, res) => {
-
-    res.send('Hello,Blog')
-
-
-
+    // 渲染模板--向客户端做出响应
+    res.render('home/index');
+})
+app.get('/about', (req, res) => {
+    // 渲染模板--向客户端做出响应
+    res.render('home/about');
+})
+app.get('/article', (req, res) => {
+    // 渲染模板--向客户端做出响应
+    res.render('home/article');
 })
 
 
